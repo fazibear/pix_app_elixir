ec8e50e566f2970b3b15524fbd9914261eef7b40
Michał Kalbarczyk <fazibear@gmail.com>
2017-12-11 00:53:45 +0100

    WIP

--- apps/matrix/src/matrix.c
+++ apps/matrix/src/matrix.c
@@ -8,7 +8,7 @@
 void set_realtime(void) {
     struct sched_param sp;
     memset(&sp, 0, sizeof(sp));
-    sp.sched_priority = sched_get_priority_max(SCHED_FIFO);
+    sp.sched_priority = 40; //sched_get_priority_max(SCHED_FIFO);
     sched_setscheduler(0, SCHED_FIFO, &sp);
     mlockall(MCL_CURRENT | MCL_FUTURE);
 }
