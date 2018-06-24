0c1a0d0d5b191f4522604993ebeeeaa14207ae76
Michał Kalbarczyk <fazibear@gmail.com>
2018-06-24 16:27:08 +0200

    Add ssh, fix matrix

--- apps/firmware/lib/firmware.ex
+++ apps/firmware/lib/firmware.ex
@@ -4,10 +4,29 @@ defmodule Firmware do
   """
 
   def init() do
+    start_ssh()
     start_ntp()
   end
 
   def start_ntp() do
     System.cmd("ntpd", ["-p", "pool.ntp.org"])
+  end
+
+  def start_ssh() do
+    authorized_keys =
+      Application.get_env(:nerves_firmware_ssh, :authorized_keys, [])
+      |> Enum.join("\n")
+
+    decoded_authorized_keys = :public_key.ssh_decode(authorized_keys, :auth_keys)
+
+    cb_opts = [authorized_keys: decoded_authorized_keys]
+    system_dir = :code.priv_dir(:nerves_firmware_ssh)
+
+    {:ok, _ref} =
+      :ssh.daemon(22, [
+        {:key_cb, {Nerves.Firmware.SSH.Keys, cb_opts}},
+        {:system_dir, system_dir},
+        shell: {IEx, :start, []}
+      ])
   end
 end
