b302e5ed93322d38ed57afd44c7330036d5e0e8a
Michał Kalbarczyk <fazibear@gmail.com>
2018-06-24 20:09:16 +0200

    Add letters

--- apps/display/lib/display/draw/chars.ex
+++ apps/display/lib/display/draw/chars.ex
@@ -15,6 +15,18 @@ defmodule Display.Draw.Char do
     ]
   end
 
+  def _0 do
+    [
+      ' cc',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'ccc'
+    ]
+  end
+
   def _1 do
     [
       '  c',
@@ -29,10 +41,10 @@ defmodule Display.Draw.Char do
 
   def _2 do
     [
-      'ccc',
+      'cc ',
       '  c',
       '  c',
-      'ccc',
+      ' c ',
       'c  ',
       'c  ',
       'ccc'
@@ -71,13 +83,13 @@ defmodule Display.Draw.Char do
       'ccc',
       '  c',
       '  c',
-      'ccc'
+      'cc '
     ]
   end
 
   def _6 do
     [
-      'ccc',
+      ' cc',
       'c  ',
       'c  ',
       'ccc',
@@ -101,7 +113,7 @@ defmodule Display.Draw.Char do
 
   def _8 do
     [
-      'ccc',
+      ' cc',
       'c c',
       'c c',
       'ccc',
@@ -113,24 +125,312 @@ defmodule Display.Draw.Char do
 
   def _9 do
     [
-      'ccc',
+      ' cc',
       'c c',
       'c c',
       'ccc',
+      '  c',
+      '  c',
+      'cc '
+    ]
+  end
+
+  def _a do
+    [
+      ' cc',
+      'c c',
+      'c c',
+      'ccc',
+      'c c',
+      'c c',
+      'c c'
+    ]
+  end
+
+  def _b do
+    [
+      ' cc',
+      'c c',
+      'c c',
+      'cc ',
+      'c c',
+      'c c',
+      'ccc'
+    ]
+  end
+
+  def _c do
+    [
+      ' cc',
+      'c  ',
+      'c  ',
+      'c  ',
+      'c  ',
+      'c  ',
+      'ccc'
+    ]
+  end
+
+  def _d do
+    [
+      'cc ',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'ccc'
+    ]
+  end
+
+  def _e do
+    [
+      ' cc',
+      'c  ',
+      'c  ',
+      'ccc',
+      'c  ',
+      'c  ',
+      'ccc'
+    ]
+  end
+
+  def _f do
+    [
+      ' cc',
+      'c  ',
+      'c  ',
+      'ccc',
+      'c  ',
+      'c  ',
+      'c  '
+    ]
+  end
+
+  def _g do
+    [
+      ' cc',
+      'c  ',
+      'c  ',
+      'c c',
+      'c c',
+      'c c',
+      'cc '
+    ]
+  end
+
+  def _h do
+    [
+      'c c',
+      'c c',
+      'c c',
+      'ccc',
+      'c c',
+      'c c',
+      'c c'
+    ]
+  end
+
+  def _i do
+    [
+      ' c ',
+      ' c ',
+      ' c ',
+      ' c ',
+      ' c ',
+      ' c ',
+      ' c '
+    ]
+  end
+
+  def _j do
+    [
+      '  c',
+      '  c',
+      '  c',
+      '  c',
+      '  c',
+      '  c',
+      'cc '
+    ]
+  end
+
+  def _k do
+    [
+      'c c',
+      'c c',
+      'c c',
+      'cc ',
+      'c c',
+      'c c',
+      'c c'
+    ]
+  end
+
+  def _m do
+    [
+      'c c',
+      'ccc',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c'
+    ]
+  end
+
+  def _n do
+    [
+      'c  ',
+      'cc ',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c'
+    ]
+  end
+
+  def _o do
+    [
+      ' cc',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'ccc'
+    ]
+  end
+
+  def _p do
+    [
+      ' cc',
+      'c c',
+      'c c',
+      'cc ',
+      'c  ',
+      'c  ',
+      'c  '
+    ]
+  end
+
+  def _q do
+    [
+      ' cc',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'cc ',
+      ' cc'
+    ]
+  end
+
+  def _r do
+    [
+      ' cc',
+      'c c',
+      'c c',
+      'cc ',
+      'c c',
+      'c c',
+      'c c'
+    ]
+  end
+
+  def _s do
+    [
+      ' cc',
+      'c  ',
+      'c  ',
+      ' c ',
       '  c',
       '  c',
       'ccc'
     ]
   end
 
-  def _0 do
+  def _t do
     [
       'ccc',
+      ' c ',
+      ' c ',
+      ' c ',
+      ' c ',
+      ' c ',
+      ' c '
+    ]
+  end
+
+  def _u do
+    [
       'c c',
       'c c',
       'c c',
       'c c',
       'c c',
+      'c c',
+      'ccc'
+    ]
+  end
+
+  def _v do
+    [
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      ' c '
+    ]
+  end
+
+  def _w do
+    [
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'c c',
+      'ccc',
+      'c c'
+    ]
+  end
+
+  def _x do
+    [
+      'c c',
+      'c c',
+      'c c',
+      ' c ',
+      'c c',
+      'c c',
+      'c c'
+    ]
+  end
+
+  def _y do
+    [
+      'c c',
+      'c c',
+      'c c',
+      'ccc',
+      ' c ',
+      ' c ',
+      ' c '
+    ]
+  end
+
+  def _z do
+    [
+      'ccc',
+      '  c',
+      '  c',
+      ' c ',
+      'c  ',
+      'c  ',
       'ccc'
     ]
   end
